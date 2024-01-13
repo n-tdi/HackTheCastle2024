5cd114a2c0974a74d5df19bbbaae4d408882d1e4
n-tdi <yogikngyt@gmail.com>
2024-01-13 09:27:59 -0500

    add java op

---
+++ HackTheJava/src/main/java/world/ntdi/hackthejava/HackTheJavaApplication.java
@@ -0,0 +1,13 @@
+package world.ntdi.hackthejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackTheJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackTheJavaApplication.class, args);
+    }
+
+}
