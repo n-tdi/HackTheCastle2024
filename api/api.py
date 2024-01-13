cfb80665408a42c5200d3105b7fb3a8f74da37f6
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2024-01-13 16:53:57 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,19 +1,39 @@
 from flask import Flask, request
 from numpy import loadtxt
-from tf import keras
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 # import tf 
 
 
-model = tf.keras.load_model('cost_model.h5')
+model = tf.keras.models.load_model('cost_model.h5')
 model.summary()
 
 app = Flask(__name__)
 
 @app.route("/api/predict")
 def hello_world():
+
     args = request.args
+    
     print(args)
     insurance = args['ins']
     descrpition = args['desc']
-    return model.predict(insurance, descrpition), 200
+
+    args = request.args
+    user_payer_input = insurance
+    user_description_input = descrpition
+
+    # Preprocess payer input
+    user_payer_input = tf.constant([user_payer_input])
+
+    # Preprocess description input
+    user_description_input = tf.constant([user_description_input])
+
+    user_inputs = {
+        "payer": user_payer_input,
+        "description": user_description_input
+    }
+
+    return model.predict(user_inputs), 200
 
